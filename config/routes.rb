2ad534fe91a40a03a02b208862c12d3dc8965036
Teerth Patel <teerth1205@gmail.com>
2020-04-06 21:42:20 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists, only: [:index]
+  
+  resources :artists do 
+  resources :songs, only: [:index, :show]
+  end 
+  
   resources :songs
+  
+ # resources :songs, only: [:index, :show]
 end
