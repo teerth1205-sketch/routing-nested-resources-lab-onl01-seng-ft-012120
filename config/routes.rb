9c089edd9d7c9582aeb38ddb7b5e55ec1dc12f12
Teerth Patel <teerth1205@gmail.com>
2020-04-06 21:32:54 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :artists
+  
+  resources :artists do 
+  resources :songs, only: [:index, :show]
+  end 
+  
   resources :songs
+ # resources :songs, only: [:index, :show]
 end
